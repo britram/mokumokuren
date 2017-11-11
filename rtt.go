fa41b28904d8eb4fe00fbc0a0e6f44dfc01d0386
Brian Trammell <brian@trammell.ch>
2017-11-11 15:37:41 +0800

    running TCP RTT tracking "works"

--- rtt.go
+++ rtt.go
@@ -1,6 +1,7 @@
 package mokumokuren
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/google/gopacket"
@@ -27,10 +28,23 @@ type RTTData struct {
 	// Number of RTT samples
 	RTTSampleCount uint
 
-	initialTCPTimestamp bool
-	useTCPTimestamps    bool
-	awaitTime           [2]*time.Time
-	awaitVal            [2]uint32
+	tcpTimestampSeen   [2]bool
+	UsingTCPTimestamps bool
+
+	component [2]time.Duration
+	awaitTime [2]*time.Time
+	awaitVal  [2]uint32
+}
+
+func extractTimestamp(tcp *layers.TCP) (uint32, uint32, bool) {
+	for _, opt := range tcp.Options {
+		if opt.OptionType == layers.TCPOptionKindTimestamps && len(opt.OptionData) >= 10 {
+			tsval := binary.BigEndian.Uint32(opt.OptionData[2:6])
+			tsecr := binary.BigEndian.Uint32(opt.OptionData[6:10])
+			return tsval, tsecr, true
+		}
+	}
+	return 0, 0, false
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -44,17 +58,85 @@ func rttInit(fe *FlowEntry, pe *PacketEvent) bool {
 	return true
 }
 
+func wrapCompare(a, b uint32) int {
+	if a == b {
+		return 0
+	} else if ((a - b) & 0x80000000) > 0 {
+		return -1
+	} else {
+		return 1
+	}
+}
+
+func sampleRTT(d *RTTData) {
+	sample := d.component[0] + d.component[1]
+	if d.RTTSampleCount == 0 {
+		d.MinimumRTT = sample
+		d.MeanRTT = sample
+	} else {
+		if sample < d.MinimumRTT {
+			d.MinimumRTT = sample
+		}
+		d.MeanRTT = ((d.MeanRTT * time.Duration(d.RTTSampleCount)) + sample) / time.Duration(d.RTTSampleCount+1)
+
+	}
+	d.RTTSampleCount += 1
+}
+
 // for checking TCP handshake RTT
 func rttTCPPacket(fe *FlowEntry, pe *PacketEvent, layer gopacket.Layer) bool {
 	tcp := layer.(*layers.TCP)
-	rttdata := fe.Data[RTTDataIndex].(*RTTData)
+	d := fe.Data[RTTDataIndex].(*RTTData)
 
 	// Calculate handshake RTT
-	if rttdata.firstReverse == nil && pe.Reverse && tcp.SYN && tcp.ACK {
-		rttdata.firstReverse = pe.Timestamp
-	} else if rttdata.secondForward == nil && !pe.Reverse && !tcp.SYN {
-		rttdata.secondForward = pe.Timestamp
-		rttdata.HandshakeRTT = rttdata.secondForward.Sub(*fe.StartTime)
+	if d.firstReverse == nil && pe.Reverse && tcp.SYN && tcp.ACK {
+		d.firstReverse = pe.Timestamp
+	} else if d.secondForward == nil && !pe.Reverse && !tcp.SYN {
+		d.secondForward = pe.Timestamp
+		d.HandshakeRTT = d.secondForward.Sub(*fe.StartTime)
+	}
+
+	dir := 0
+	rdir := 1
+	if pe.Reverse {
+		dir = 1
+		rdir = 0
+	}
+
+	// try to get a timestamp
+	tsval, tsecr, tsok := extractTimestamp(tcp)
+
+	// check to see if we'd like to switch to timestamps
+	if tsok {
+		d.tcpTimestampSeen[dir] = true
+
+		if d.tcpTimestampSeen[0] && d.tcpTimestampSeen[1] && !d.UsingTCPTimestamps {
+			d.UsingTCPTimestamps = true
+			d.awaitTime[0] = nil
+			d.awaitTime[1] = nil
+			d.awaitVal[0] = 0
+			d.awaitVal[1] = 0
+		}
+	}
+
+	// check to see if we're waiting for a value that we have
+	if d.awaitTime[dir] != nil {
+		if (d.UsingTCPTimestamps && wrapCompare(tsecr, d.awaitVal[dir]) > 0) ||
+			(!d.UsingTCPTimestamps && wrapCompare(tcp.Ack, d.awaitVal[dir]) > 0) {
+			d.component[dir] = pe.Timestamp.Sub(*d.awaitTime[dir])
+			d.awaitTime[dir] = nil
+			sampleRTT(d)
+		}
+	}
+
+	// check to see if we need to grab a new value to wait for
+	if d.awaitTime[rdir] == nil {
+		d.awaitTime[rdir] = pe.Timestamp
+		if d.UsingTCPTimestamps {
+			d.awaitVal[rdir] = tsval
+		} else {
+			d.awaitVal[rdir] = tcp.Seq
+		}
 	}
 
 	return true
--- rtt_test.go
+++ rtt_test.go
@@ -14,6 +14,8 @@ import (
 type ExpectedRTTMetrics struct {
 	Handshake time.Duration
 	Minimum   time.Duration
+	Mean      time.Duration
+	Count     uint
 }
 
 type ExpectedFlowRTTs map[moku.FlowKey]ExpectedRTTMetrics
@@ -37,19 +39,27 @@ func rttVerificationEmitter(t *testing.T, filename string, e ExpectedFlowRTTs) m
 
 		if DumpExpectedFlows {
 			fmt.Fprintf(RTTEmitterLog, "// in file %s\n", filename)
-			fmt.Fprintf(RTTEmitterLog, "{\"%s\",\"%s\",%d,%d,%d}: ExpectedRTTMetrics{%d,%d}\n",
+			fmt.Fprintf(RTTEmitterLog, "{\"%s\",\"%s\",%d,%d,%d}: ExpectedRTTMetrics{%d,%d,%d,%d}\n",
 				fe.Key.Sip, fe.Key.Dip, fe.Key.Sp, fe.Key.Dp, fe.Key.P,
-				rttdata.HandshakeRTT, rttdata.MinimumRTT)
+				rttdata.HandshakeRTT, rttdata.MinimumRTT, rttdata.MeanRTT, rttdata.RTTSampleCount)
 		}
 
 		metrics, ok := e[fe.Key]
 		if ok {
 			if metrics.Handshake != rttdata.HandshakeRTT {
-				t.Logf("flow %s expected hrtt %d got %d", fe.Key, metrics.Handshake, rttdata.HandshakeRTT)
+				t.Logf("flow %s expected hsrtt %d got %d", fe.Key, metrics.Handshake, rttdata.HandshakeRTT)
 				t.Fail()
 			}
 			if metrics.Minimum != rttdata.MinimumRTT {
 				t.Logf("flow %s expected minrtt %d got %d", fe.Key, metrics.Minimum, rttdata.MinimumRTT)
+				t.Fail()
+			}
+			if metrics.Mean != rttdata.MeanRTT {
+				t.Logf("flow %s expected meanrtt %d got %d", fe.Key, metrics.Mean, rttdata.MeanRTT)
+				t.Fail()
+			}
+			if metrics.Count != rttdata.RTTSampleCount {
+				t.Logf("flow %s expected samplecount %d got %d", fe.Key, metrics.Count, rttdata.RTTSampleCount)
 				t.Fail()
 			}
 		}
@@ -64,7 +74,7 @@ func TestRTTMeasurement(t *testing.T) {
 	}{
 		{"testdata/magpie_v6.pcap",
 			ExpectedFlowRTTs{
-				{"2001:67c:370:128:10b8:6449:2dbf:4fc", "2a03:b0c0:3:d0::27a1:1", 61040, 443, 6}: ExpectedRTTMetrics{183653000, 0},
+				{"2001:67c:370:128:10b8:6449:2dbf:4fc", "2a03:b0c0:3:d0::27a1:1", 61040, 443, 6}: ExpectedRTTMetrics{183653000, 183555000, 216679227, 13},
 			},
 		},
 	}
